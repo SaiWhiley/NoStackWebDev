12155e70b3a861315c9bef0a2ca9fb03350b7307
SaiWhiley <sai.whiley196@gmail.com>
2017-05-15 23:56:38 +1000

    php header

---
+++ stage2/index.php
@@ -0,0 +1,9 @@
+<head>
+  <title>Brisbane Parks</title>
+  <link href="generalStyles.css" rel="stylesheet" type="text/css">
+  <link href="indexStyles.css" rel="stylesheet" type="text/css">
+</head>
+
+<?php
+    include "header.inc";
+?>
