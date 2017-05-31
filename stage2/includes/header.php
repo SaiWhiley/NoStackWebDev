f42eb476d3d492648c71e3d815dd6cec11dc81e5
Jemima Weir <Jemima@Jemimas-MacBook-Pro.local>
2017-05-31 16:29:15 +1000

    xss stopped

--- stage2/includes/header.php
+++ stage2/includes/header.php
@@ -2,6 +2,6 @@
     <h1>Brisbane's Public Parks</h1>
     <div id="menu">
         <a href = "index.php">Home</a><a href="locations.php">Locations</a>
-        <a href="registration.php">Register</a><a href="login.php">Login</a>
+        <a href="registration.php">Register</a><a href="login.php">Login</a><a href="logout.php">Logout</a>
     </div>
 </div>
--- stage2/singleItem.php
+++ stage2/singleItem.php
@@ -1,4 +1,41 @@
+<!-- xss cleansing function from https://stackoverflow.com/questions/1996122/how-to-prevent-xss-with-html-php -->
+
 <?php
+function xss_clean($data) {
+  // Fix &entity\n;
+  $data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+  $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+  $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+  $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+
+  // Remove any attribute starting with "on" or xmlns
+  $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+
+  // Remove javascript: and vbscript: protocols
+  $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+  $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+  $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+
+  // Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+  $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+  $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+  $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+
+  // Remove namespaced elements (we do not need them)
+  $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+
+  do
+  {
+      // Remove really unwanted tags
+      $old_data = $data;
+      $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+  }
+  while ($old_data !== $data);
+
+  // we are done...
+  return $data;
+}
+
   include_once "includes/PDO.php";
   //include_once "login_session.inc";
   if (/*isset($_SESSION['username']) && */isset($_POST['comments'])) {
@@ -7,7 +44,8 @@
       $stmtIns = $pdo->prepare('INSERT INTO reviews (Username, Rating, Comments, parkID, DateLeft) VALUES ( "testusername" , :rating , :comments, :id, :dateleft);');
       //$stmtIns->bindValue(':username', $_SESSION['username']);
       $stmtIns->bindValue(':rating', $_POST['rating']);
-      $stmtIns->bindValue(':comments', $_POST['comments']);
+      $comment = xss_clean($_POST['comments']);
+      $stmtIns->bindValue(':comments', $comment);
 	    $stmtIns->bindValue(':id', $_GET['id']);
       $stmtIns->bindValue(':dateleft', $date);
       $stmtIns->execute();
