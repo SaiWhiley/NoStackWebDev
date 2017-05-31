a4dcfbfc556f8c2181453fc129ae30a0dba5e9ad
SaiWhiley <sai.whiley196@gmail.com>
2017-05-31 13:59:32 +1000

    reviews submit and display

--- stage2/singleItem.php
+++ stage2/singleItem.php
@@ -1,15 +1,16 @@
 <?php
   include_once "includes/PDO.php";
   //include_once "login_session.inc";
-  if (isset($_SESSION['username']) && isset($_POST['comments'])) {
+  if (/*isset($_SESSION['username']) && */isset($_POST['comments'])) {
     try {
 		echo 'Review Submitted';
-      $stmtIns = $pdo->prepare('INSERT INTO reviews (Username, Rating, Comments, parkID) VALUES ( :username , 0 , :comments, :id);');
-		
-      $stmtIns->bindValue(':username', $_SESSION['username']);
-      //$stmt->bindValue(':time', $currentTime);
+      $date = date("Y-m-d");
+      $stmtIns = $pdo->prepare('INSERT INTO reviews (Username, Rating, Comments, parkID, DateLeft) VALUES ( "testusername" , :rating , :comments, :id, :dateleft);');
+      //$stmtIns->bindValue(':username', $_SESSION['username']);
+      $stmtIns->bindValue(':rating', $_POST['rating']);
       $stmtIns->bindValue(':comments', $_POST['comments']);
-	  $stmtIns->bindValue(':id', $_GET['id']);
+	    $stmtIns->bindValue(':id', $_GET['id']);
+      $stmtIns->bindValue(':dateleft', $date);
       $stmtIns->execute();
       }
       catch (PDOException $e){
@@ -24,7 +25,6 @@
   <title>Brisbane Parks</title>
   <link href="globalStyles.css" rel="stylesheet" type="text/css">
   <link href="singleItemStyles.css" rel="stylesheet" type="text/css">
-  <!--TODO <script src="votepage.js"></script> -->
   <?php include "includes/metaData.php"; ?>
 </head>
 <body>
@@ -57,11 +57,16 @@
     ?>
     <div id="writeComment">
         <form method="post">
-            <table>
-                <!--vote/5-->
-            </table>
-            <p>Leave a Review: </p>
-            <textarea name="comments" rows = "3" cols="89"></textarea>
+            <p>Leave a Review:</p>
+            <p>Rating:
+              <select id="rating" name="rating" value="">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </p>
+            <textarea name="comments" rows = "3" cols="89" placeholder="Comments:"></textarea>
             <button type="submit">Leave Review</button>
         </form>
     </div>
@@ -69,7 +74,7 @@
       <table>
         <?php
           foreach ($reviewResults as $data) {
-            echo '<tr><td><h4>'.$data['Username'].' said:</h4><p>'.$data['Comments'].'</p></td></tr>';
+            echo '<tr><td><h4>'.$data['Rating'].'/5 '.$data['Username'].' said:</h4><p>'.$data['Comments'].'</p></td></tr>';
           }
         ?>
       </table>
